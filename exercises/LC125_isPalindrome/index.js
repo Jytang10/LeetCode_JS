9ed1f3a3913712353fd8d4c6921362440ec7a642
James Tang <jytang10@gmail.com>
2019-11-27 17:12:53 -0800

    Completed LC125

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,16 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+  const sanitizeStr = s.toLowerCase().replace(/[\W_]/g, '');
+  let leftPointer = 0;
+  let rightPointer = sanitizeStr.length - 1;
+
+  while(leftPointer < rightPointer){
+    if(sanitizeStr[leftPointer] !== sanitizeStr[rightPointer]){
+      return false;
+    }
+    leftPointer++;
+    rightPointer--;
+  }
+  return true;
+}
 
 module.exports = isPalindrome;
