8f3b50dae901e4e349f8ce3a5502c5a569fa6504
James Tang <jytang10@gmail.com>
2019-11-29 20:11:24 -0800

    Completed LC5

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,10 @@
+// Pseudocode
+// 1. Sanitize input string by removing non alphanumeric characters and lowercasing it
+// 2. Create a left and right pointer, initially at start and end of input string
+// 3. While left pointer is less than right pointer
+//    If characters at left pointer and right pointer are not equal, return false
+// 4. Return true
+
 function isPalindrome(s) {
   const sanitizeStr = s.toLowerCase().replace(/[\W_]/g, '');
   let leftPointer = 0;
--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,44 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+//*Pseudocode
+// 1. Define start index
+// 2. Define max length
+// 3. Createa helper function, expandAroundMiddle, with left and right input params
+//    While the left is greater than or equal to 0, right is less than the length of the string, and left index of string is equal to right index of string
+//      Define current palindrome length as right minus left plus 1 (account for arrays 0 index)
+//      If the current palindrome length is greater than the max length
+//        Set the current palindrome length as the new max length
+//        Set the start index as left
+//    Decrement left by 1
+//    Increment right by 1
+// 4. Loop through each character in the string
+//    Run helper function for odd-character strings (inputs assuming that the center is a character and left and right are adjacent characters)
+//    Run helper function for even-character strings (inputs assuming that the center is the space between character, and left and right are the index and index + 1)
+// 5. Return the sliced portion of the string between the start index and the sum of the start index and the max length
+
+function longestPalindrome(s) {
+  let startIndex = 0;
+  let maxLength = 1;
+
+  function expandAroundMiddle(left, right){
+    while(left >= 0 && right < s.length && s[left] === s[right]){
+      const currentPalLength = right - left + 1;
+      if(currentPalLength > maxLength){
+        maxLength = currentPalLength;
+        startIndex = left;
+      }
+      left -= 1;
+      right += 1;
+    }
+  }
+
+  for(let i = 0; i < s.length; i++){
+    expandAroundMiddle(i - 1, i + 1);
+    expandAroundMiddle(i, i + 1);
+  }
+
+  return s.slice(startIndex, startIndex + maxLength);
+}
 
 module.exports = longestPalindrome;
