0e7d1f391b67be5cc2ef97f735469b2e2e3bb8eb
James Tang <jytang10@gmail.com>
2019-12-04 14:40:49 -0800

    Completed LC206

--- exercises/LC206_reverseList/index.js
+++ exercises/LC206_reverseList/index.js
@@ -1,3 +1,32 @@
-const reverseList = head => {};
+// Time Complexity: O(N). Traversed through Linked List once
+// Space Complexity: O(1)
+
+// Psuedocode
+// 1. Create prev variable, initially set to null
+// 2. Create current variable, initially set to the head
+// 3. Run a while loop
+//    While current is not null
+//      Create a temp variable, set to current.next
+//      Change the direction by setting current.next equal to prev
+//      Move prev and current up by 1. Set prev equal to current, set current equal to temp
+// 4. Return the final resulting head, prev
+
+const reverseList = head => {
+  let prev = null;
+  let current = head;
+
+  while (current !== null) {
+    let temp = current.next
+
+    // Change direction
+    current.next = prev;
+
+    // Move prev and current up by 1
+    prev = current;
+    current = temp;
+  }
+
+  return prev;
+};
 
 module.exports = reverseList;
