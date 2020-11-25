38a5716fa39085884497b5964a1192cb9ccf09df
James Tang <jytang10@gmail.com>
2020-11-25 19:05:06 +0900

    completed LC15

--- exercises/LC15_threeSum/index.js
+++ exercises/LC15_threeSum/index.js
@@ -1,3 +1,31 @@
-const threeSum = nums => {};
+const threeSum = (nums) => {
+  const res = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i++) {
+    const target = 0 - nums[i];
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+
+    while (left < right) {
+      if (nums[left] + nums[right] === target) {
+        res.push([nums[i], nums[left], nums[right]]);
+        left++;
+        while (nums[left] === nums[left - 1]) {
+          left++;
+        }
+      } else if (nums[left] + nums[right] < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return res;
+};
 
 module.exports = threeSum;
